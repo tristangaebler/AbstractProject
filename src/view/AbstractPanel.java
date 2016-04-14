f0bc0739c88cb556dba7b38b3ca4789b55a385a3
tristangaebler <Tristan.Gaebler@gmail.com>
2016-04-14 09:55:18 -0600

    addded GUI

--- src/view/AbstractPanel.java
+++ src/view/AbstractPanel.java
@@ -18,23 +18,25 @@ public class AbstractPanel extends JPanel
 		abstractButton = new JButton("Abstract Button");
 		textField = new JTextField("Abstract Text Field");
 		baseLayout = new SpringLayout();
-	}
-	
-	private void setUpPane()
-	{
 		
+		
+		//Calling methods
+		setUpPanel();
 	}
 	
 	private void setUpPanel()
 	{
 		this.setLayout(baseLayout);
 		this.add(abstractButton);
-		this.add()
+		this.add(textField);
 	}    
 	
 	private void setUpLayout()
 	{
-		
+		baseLayout.putConstraint(SpringLayout.WEST, textField, 131, SpringLayout.WEST, this);
+		baseLayout.putConstraint(SpringLayout.NORTH, abstractButton, 38, SpringLayout.SOUTH, textField);
+		baseLayout.putConstraint(SpringLayout.WEST, abstractButton, 0, SpringLayout.WEST, textField);
+		baseLayout.putConstraint(SpringLayout.NORTH, textField, 69, SpringLayout.NORTH, this);
 	}
 	
 	private void setUpListeners()
