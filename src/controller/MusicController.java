c744d3945777d0b693aa0b38a8c8fa96c1256ea8
tristangaebler <Tristan.Gaebler@gmail.com>
2016-04-14 08:31:00 -0600

    Wrote quickSort algorithm

--- src/controller/MusicController.java
+++ src/controller/MusicController.java
@@ -31,8 +31,40 @@ public class MusicController
 			while(innerLoop > 0 && (ironMan.get(innerLoop - 1).comapreTo(ironMan.get(innerLoop))) > 0)
 			{
 				swap(innerLoop, innerLoop - 1);
+				innerLoop--;
 			}
 		}
+	}
+	
+	public void quicksort(int low, int high)
+	{
+		if(low < high)
+		{
+			int midPoint = partition(low, high);
+			quicksort(low, midPoint - 1);
+			quicksort(midPoint + 1, high);
+		}
+	}
+	
+	
+	private int partition(int low, int high)
+	{
+		int partitionPoint = 0;
+		
+		IronMan pivot = ironMan.get(high);
+		int position = low;
+		for(int index = low; index < high - 1; index++)
+		{
+			if(ironMan.get(index).comapreTo(pivot) <= 0)
+			{
+				swap(position, index);
+				position++;
+			}
+		}
+		
+		swap(position, high);
+		
+		return position;
 	}
 
 }
